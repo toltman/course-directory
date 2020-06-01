a4779f3e69d5ccccfeaf502a0dac4cbc01acd392
Tim Oltman <toltman@gmail.com>
2020-06-01 12:13:51 -0400

    Updates

---
+++ precache-manifest.d3f684ce6348bc9659c5d1d7cb3600ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d83fe16726ef33db6ad3b1c9dc7205",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "1a0e46abc53ec1dd3df1",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "734893c7cf4a0af4f6a2",
+    "url": "/course-directory/static/js/2.bd60fe78.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/course-directory/static/js/2.bd60fe78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0e46abc53ec1dd3df1",
+    "url": "/course-directory/static/js/main.0ea43507.chunk.js"
+  },
+  {
+    "revision": "6ce79fa6db6b086a4e3e",
+    "url": "/course-directory/static/js/runtime-main.5dec270c.js"
+  }
+]);
